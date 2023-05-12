cb705aff07b1e2103e55ccc822fb86421bdf538d
Felipe Fabossi <felipefabossi@Jotfsans-MacBook-Pro.local>
2023-05-12 19:05:01 -0300

    Some style

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,12 +7,7 @@ export const environment = {
   auth: {
     domain: 'dev-6g6o3em3hlydzt3e.us.auth0.com',
     clientId: 'piPEJNbL3mP712iuc8m3fcOWlaF9YNFz',
-  },
-  iproov: {
-    apiUrl: '<sua_url_do_api_iproov>',
-    apiKey: '<sua_chave_de_api_iproov>',
-    apiSecret: '<seu_secreto_do_cliente_iproov>',
-  },
+  }
 };
 
 /*
